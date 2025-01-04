c57451d90633fb13328477382276110c8f782997
joaopfmonteiro <joaoosverdes@gmail.com>
2025-01-04 15:13:22 +0100

    first commit

---
+++ src/main/java/com/trainWise/app/TrainWiseApplication.java
@@ -0,0 +1,13 @@
+package com.trainWise.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainWiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainWiseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trainWise/app/TrainWiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trainWise.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainWiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
